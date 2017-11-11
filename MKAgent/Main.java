c81a8d3dea13b8a1f50fab0fec6bf1b822cf55e7
Sami Alabed <sami.alabed@student.manchester.ac.uk>
2017-11-11 16:03:25 +0000

    Add initial run agent code

--- MKAgent/Main.java
+++ MKAgent/Main.java
@@ -1,4 +1,5 @@
 package MKAgent;
+
 import java.io.BufferedReader;
 import java.io.EOFException;
 import java.io.IOException;
@@ -9,8 +10,7 @@ import java.io.Reader;
  * The main application class. It also provides methods for communication
  * with the game engine.
  */
-public class Main
-{
+public class Main {
     /**
      * Input from the game engine.
      */
@@ -18,42 +18,75 @@ public class Main
 
     /**
      * Sends a message to the game engine.
+     *
      * @param msg The message.
      */
-    public static void sendMsg (String msg)
-    {
-    	System.out.print(msg);
-    	System.out.flush();
+    public static void sendMsg(String msg) {
+        System.out.print(msg);
+        System.out.flush();
     }
 
     /**
      * Receives a message from the game engine. Messages are terminated by
      * a '\n' character.
+     *
      * @return The message.
      * @throws IOException if there has been an I/O error.
      */
-    public static String recvMsg() throws IOException
-    {
-    	StringBuilder message = new StringBuilder();
-    	int newCharacter;
+    public static String recvMsg() throws IOException {
+        StringBuilder message = new StringBuilder();
+        int newCharacter;
 
-    	do
-    	{
-    		newCharacter = input.read();
-    		if (newCharacter == -1)
-    			throw new EOFException("Input ended unexpectedly.");
-    		message.append((char)newCharacter);
-    	} while((char)newCharacter != '\n');
+        do {
+            newCharacter = input.read();
+            if (newCharacter == -1)
+                throw new EOFException("Input ended unexpectedly.");
+            message.append((char) newCharacter);
+        } while ((char) newCharacter != '\n');
 
-		return message.toString();
+        return message.toString();
     }
 
-	/**
-	 * The main method, invoked when the program is started.
-	 * @param args Command line arguments.
-	 */
-	public static void main(String[] args)
-	{
-		// TODO: implement
-	}
+    /**
+     * The main method, invoked when the program is started.
+     *
+     * @param args Command line arguments.
+     */
+    public static void main(String[] args) {
+        try {
+            String s;
+            while (true) {
+                System.err.println();
+                s = recvMsg();
+                System.err.print("Received: " + s);
+                try {
+                    MsgType mt = Protocol.getMessageType(s);
+                    switch (mt) {
+                        case START:
+                            System.err.println("A start.");
+                            boolean first = Protocol.interpretStartMsg(s);
+                            System.err.println("Starting player? " + first);
+                            break;
+                        case STATE:
+                            System.err.println("A state.");
+                            Board b = new Board(7, 7);
+                            Protocol.MoveTurn r = Protocol.interpretStateMsg(s, b);
+                            System.err.println("This was the move: " + r.move);
+                            System.err.println("Is the game over? " + r.end);
+                            if (!r.end) System.err.println("Is it our turn again? " + r.again);
+                            System.err.print("The board:\n" + b);
+                            break;
+                        case END:
+                            System.err.println("An end. Bye bye!");
+                            return;
+                    }
+
+                } catch (InvalidMessageException e) {
+                    System.err.println(e.getMessage());
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("This shouldn't happen: " + e.getMessage());
+        }
+    }
 }
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # KalahAI
 An AI agent for the board game Kalah
+
+
+# Instructions
+To play the game, first start the Game Engine
+
+1.  Start the game engine `java -jar ManKalah.jar "nc localhost 12345" "java -jar <Name of second agent to play the game>.jar"`
+2. Connect to the game engine by opening a window (e.g. using xterm) and type in this window
+` nc -l localhost 12345`
