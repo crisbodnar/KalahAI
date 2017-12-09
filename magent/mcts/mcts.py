1b651a36008c5c958e50ecdf9445ddac95945714
Sami Alabed <sami.alabed@student.manchester.ac.uk>
2017-12-09 20:13:48 +0000

    Add policy and value network accessor

--- magent/mcts/mcts.py
+++ magent/mcts/mcts.py
@@ -1,10 +1,9 @@
 import datetime
 import logging
 
-from magent.board import Board
 from magent.mancala import MancalaEnv
 from magent.mcts.graph.node import Node
-from magent.mcts.graph.node_utils import select_max_child
+import magent.mcts.graph.node_utils as node_utils
 from magent.mcts.policies.policies import tree_factory, default_factory, DefaultPolicy, TreePolicy
 from magent.move import Move
 
@@ -21,19 +20,19 @@ class MCTS(object):
         games_played = 0
         while datetime.datetime.utcnow() - start_time < self.calculation_time:
             node = self.tree_policy.select(game_state_root)
-            reward = self.default_policy.simulate(node) / 1000.0
+            reward = self.default_policy.simulate(node)
             node.backpropagate(reward)
             # Debugging information
             games_played += 1
-            # logging.debug("%s; Game played %i" % (node, games_played))
+            logging.debug("%s; Game played %i" % (node, games_played))
         logging.debug("%s" % game_state_root)
-        return select_max_child(game_state_root).move
+        return node_utils.select_robust_child(game_state_root).move
 
 
 mcts_presets = {
     'standard-mcts': MCTS(tree_policy=tree_factory('monte-carlo'),
                           default_policy=default_factory('monte-carlo'),
-                          time_sec=30),
+                          time_sec=60),
     'test-mcts': MCTS(tree_policy=tree_factory('monte-carlo'),
                       default_policy=default_factory('monte-carlo'),
                       time_sec=1),
