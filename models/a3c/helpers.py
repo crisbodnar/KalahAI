66d5d4a51f4e142c112cfa9725e25957f03fa3d2
Cristian Bodnar <cristian.bodnar@manchester.ac.uk>
2017-12-11 10:49:46 +0000

    Add cjeckpoints for a3c

--- models/a3c/helpers.py
+++ models/a3c/helpers.py
@@ -39,7 +39,7 @@ def discount(x, gamma):
 
 # Used to initialize weights for policy and value output layers
 def normalized_columns_initializer(std=1.0):
-    def _initializer(shape, _dtype=None, _partition_info=None):
+    def _initializer(shape, dtype=None, partition_info=None):
         out = np.random.randn(*shape).astype(np.float32)
         out *= std / np.sqrt(np.square(out).sum(axis=0, keepdims=True))
         return tf.constant(out)
